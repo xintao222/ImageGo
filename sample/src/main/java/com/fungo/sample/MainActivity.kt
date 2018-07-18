3e7b180fd103d470d9e7503eecd2a29e396ec827
Pinger <wanitme@gmail.com>
2018-07-18 18:37:32 +0800

    更新ImageGo静态文件API

--- sample/src/main/java/com/fungo/sample/MainActivity.kt
+++ sample/src/main/java/com/fungo/sample/MainActivity.kt
@@ -14,8 +14,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        loadRound(mGif,imageView,20)
     }
 
 
@@ -24,14 +22,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    override fun onResume() {
-        super.onResume()
-        resumeRequests(this)
-    }
 
 
-    override fun onPause() {
-        super.onPause()
-        pauseRequests(this)
-    }
+
 }
